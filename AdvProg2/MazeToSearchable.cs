eeada0d7db051aee7aa9f5bd844e25159563cce3
shai <shai@LENOVOFER>
2017-04-18 04:12:30 +0300

    Added MVC

---
+++ AdvProg2/MazeToSearchable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchAlgorithmsLib;
+using MazeLib;
+
+namespace MazeSolver
+{
+    class MazeToSearchable : ISearchable
+    {
+        private Maze maze;
+        private State<Position> initial;
+        private State<Position> goal;
+
+        public MazeToSearchable(Maze inputMaze)
+        {
+            this.maze = inputMaze;
+            this.initial = new State<Position>(inputMaze.InitialPos);
+            this.goal = new State<Position>(inputMaze.GoalPos);
+        }
+
+        public List<IState> GetAllPossibleStates(IState state)
+        {
+            List<IState> neighbors = new List<IState>();
+            // Can only handle states befitting maze-type objects.
+            if (state.GetState().GetType() != typeof(Position))
+            {
+                return neighbors;
+            }
+            Position current = (Position)state.GetState();
+            int row = current.Row, col = current.Col;
+            // Adding all possible neighbors:
+            if (row > 0 && this.maze[row - 1, col] == CellType.Free)
+                neighbors.Add(new State<Position>(new Position(row - 1, col), state));
+            if (row < this.maze.Rows - 1 && this.maze[row + 1, col] == CellType.Free)
+                neighbors.Add(new State<Position>(new Position(row + 1, col), state));
+            if (col > 0 && this.maze[row, col - 1] == CellType.Free)
+                neighbors.Add(new State<Position>(new Position(row, col - 1), state));
+            if (col < this.maze.Cols - 1 && this.maze[row, col + 1] == CellType.Free)
+                neighbors.Add(new State<Position>(new Position(row, col + 1), state));
+            return neighbors;
+        }
+
+        public IState GetGoalState()
+        {
+            return goal;
+        }
+
+        public IState GetInitialState()
+        {
+            return initial;
+        }
+    }
+}
---
+++ AdvProg2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeGeneratorLib;
+using MazeLib;
+using SearchAlgorithmsLib;
+
+namespace MazeSolver
+{
+    class Program
+    {
+        static void CompareSolvers()
+        {
+            DFSMazeGenerator mazeGen = new DFSMazeGenerator();
+            Maze maze = mazeGen.Generate(100, 100);
+            Console.WriteLine(maze.ToString());
+            BestFirstSearcher bfsSrch = new BestFirstSearcher();
+            DepthFirstSearcher dfsSrch = new DepthFirstSearcher();
+            ISearchable searchableMaze = new MazeToSearchable(maze);
+            bfsSrch.Search(searchableMaze);
+            dfsSrch.Search(searchableMaze);
+            Console.WriteLine("BFS solved in: " + bfsSrch.GetNumberOfNodesEvaluated());
+            Console.WriteLine("DFS solved in: " + dfsSrch.GetNumberOfNodesEvaluated());
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            CompareSolvers();
+        }
+    }
+}
